8635d0ebe8e3194bd6f9532639b958f07230b8ef
Evy Kassirer <evy@patreon.com>
2019-01-08 14:12:59 -0800

    make changes, not tested yet

--- social_core/tests/backends/test_patreon.py
+++ social_core/tests/backends/test_patreon.py
@@ -5,7 +5,7 @@ from .oauth import OAuth2Test
 
 class PatreonOAuth2Test(OAuth2Test):
     backend_path = 'social_core.backends.patreon.PatreonOAuth2'
-    user_data_url = 'https://api.patreon.com/oauth2/api/current_user'
+    user_data_url = 'https://www.patreon.com/api/oauth2/v2/identity'
     expected_username = 'JohnInterwebs'
     access_token_body = json.dumps({
         'access_token': 'foobar',
